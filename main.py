0e98daafc14723907837728b6f18ef32d8598012
ekioktaviana <oktavianarivaldi27@gmail.com>
2021-07-18 10:08:20 +0700

    revisi 2

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import configparser
 config = configparser.RawConfigParser()
 config.read(filenames= 'my.properties')
 # print(config.sections())
-#
+
 # print(db)
 scrap_db = pymysql.connect(host='127.0.0.1',user='root',password='',port=3306,db='scraping_trending_youtube',charset = "utf8")
 cursor = scrap_db.cursor()
@@ -19,16 +19,13 @@ sql = "CREATE TABLE trendings (id INT AUTO_INCREMENT PRIMARY KEY,channel_id VARC
 cursor.execute(sql)
 
 
-# List of simple to collect features
 snippet_features = ["channelId",
                     "title",
                     "channelTitle",
                     "publishedAt"]
 
-# Any characters to exclude, generally these are things that become problematic in CSV files
 unsafe_characters = ['\n', '"']
 
-# Used to identify columns, currently hardcoded order
 header = snippet_features
 
 
@@ -43,14 +40,12 @@ def setup(api_path, code_path):
 
 
 def prepare_feature(feature):
-    # Removes any character from the unsafe characters list and surrounds the whole item in quotes
     for ch in unsafe_characters:
         feature = str(feature).replace(ch, "")
     return f'"{feature}"'
 
 
 def api_request(page_token, country_code):
-    # Builds the URL and requests the JSON from it
     request_url = f"https://www.googleapis.com/youtube/v3/videos?part=id,statistics,snippet{page_token}chart=mostPopular&regionCode={country_code}&maxResults=50&key=AIzaSyCdiHX3iHX_8UCSXp1SuaK10abCNC5M3Zs"
     request = requests.get(request_url)
     if request.status_code == 429:
@@ -60,23 +55,17 @@ def api_request(page_token, country_code):
 
 
 def get_tags(tags_list):
-    # Takes a list of tags, prepares each tag and joins them into a string by the pipe character
     return prepare_feature("|".join(tags_list))
 
 
 def get_videos(items):
     lines = []
     for video in items:
-
-        # Snippet and statistics are sub-dicts of video, containing the most useful info
         snippet = video['snippet']
 
-        # This list contains all of the features in snippet that are 1 deep and require no special processing
         features = [prepare_feature(snippet.get(feature, "")) for feature in snippet_features]
 
-        # Compiles all of the various bits of info into one consistently formatted line
         line = features
-        # print(features)
         lines.append(",".join(line))
 
         # print(line)
@@ -87,29 +76,22 @@ def get_videos(items):
         cursor = scrap_db.cursor()
         cursor.execute("INSERT INTO trendings (channel_id,title,channel_title,published_at) VALUES ({},{},{},{})".format(line[0],line[1],line[2],line[3]))
         scrap_db.commit()
-        print(cursor.rowcount, "Record inserted successfully into Trendings table")
         scrap_db.close()
         cursor.close()
 
 
+    print("Record inserted successfully into Trendings table")
     return lines
 
 
 def get_pages(country_code, next_page_token="&"):
     country_data = []
-
-    # Because the API uses page tokens (which are literally just the same function of numbers everywhere) it is much
-    # more inconvenient to iterate over pages, but that is what is done here.
     while next_page_token is not None:
-        # A page of data i.e. a list of videos and all needed data
         video_data_page = api_request(next_page_token, country_code)
 
-        # Get the next page token and build a string which can be injected into the request with it, unless it's None,
-        # then let the whole thing be None so that the loop ends after this cycle
         next_page_token = video_data_page.get("nextPageToken", None)
         next_page_token = f"&pageToken={next_page_token}&" if next_page_token is not None else next_page_token
 
-        # Get all of the items as a list and let get_videos return the needed features
         items = video_data_page.get('items', [])
         country_data += get_videos(items)
 
